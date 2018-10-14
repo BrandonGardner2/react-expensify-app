259b27f2e23dbb55c6d6f9233aba8bb7fc4805d9
Brandon G-Home <brandon.gardner2@outlook.com>
2018-10-14 09:42:57 -0500

    Removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-//install -> import -> use
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -12,28 +11,6 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-// addExpense -> Water bill
-store.dispatch(addExpense({
-    description: 'water bill',
-    amount: 4500
-}));
-
-// addExpense -> Gas bill
-store.dispatch(addExpense({
-    description: 'gas bill',
-    amount: 1500,
-    createdAt: 1000
-}));
-
-store.dispatch(addExpense({
-    description: 'rent',
-    amount: 109500,
-}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
 
 const jsx = (
     <Provider store={store}>
